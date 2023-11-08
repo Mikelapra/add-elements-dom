e5f9e1ecd179348cc5c5b30999746a73fc11a912
Mikel Apraiz <apraiz.mikel@gmail.com>
2023-11-08 23:47:04 +0100

    add JS for button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
  // Aquí tu código
+ const agregar = document.getElementById("agregar");
+ 
+ agregar.addEventListener('click', function () {
+    const lista = document.getElementById("lista")
+    const li = document.createElement('li')
+
+    const Pregunta = prompt ("Introducir elemento")
+    li.textContent = Pregunta
+    lista.append(li);
+  });
